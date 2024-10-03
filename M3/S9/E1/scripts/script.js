fc1fa0df8798dddc435a9d91f9abf86fb5f648d2
Nakajito <nakajito@proton.me>
2024-10-02 19:00:21 -0500

    se agregó la cunción para cambiar el fondo y el borde redondeado en los items

---
+++ M3/S9/E1/scripts/script.js
@@ -0,0 +1,11 @@
+$(Document).ready(function(){
+    $('#color').click(function(){
+        $('.item').css({'background-color': 'red'});
+    });
+
+    $('#borde').click(function(){
+        $('.item').css({'border-radius':'30px'})
+    });
+
+    
+});

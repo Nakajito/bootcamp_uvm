64e7c01562528bd85dc8b550530d3ee1d29d84a7
Nakajito <nakajito@proton.me>
2024-10-03 21:20:04 -0500

    se agreggó la funcion para agregar un nuevo div

--- M3/S9/E2/scripts/script.js
+++ M3/S9/E2/scripts/script.js
@@ -8,6 +8,13 @@ function CambiarColor() {
 function borde() {
     let items = document.querySelectorAll('.item');
     items.forEach(function (item) {
-        item.style.borderRadius = "20px"
+        item.style.borderRadius = "20px";
     });
+}
+
+function nuevoDiv() {
+    let nuevoDiv = document.createElement('div');
+    nuevoDiv.classList.add('item');
+    let mainContainer = document.getElementById('mainContainer');
+    mainContainer.appendChild(nuevoDiv);
 }

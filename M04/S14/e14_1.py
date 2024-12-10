062ea525d29a3e3544264d674ebbb0b4030345d9
NK <nakajito@proton.me>
2024-12-10 11:42:02 -0500

    Actualización del ejercicio 14_1

--- M04/S14/e14_1.py
+++ M04/S14/e14_1.py
@@ -6,7 +6,6 @@
         4.	Eliminar el último elemento de la lista y mostrar la lista actualizada.
 """
 
-"""
 input_user = input('Ingresa una lista de numeros enteros separados por comas: ')
 input_list = input_user.split(',')
 
@@ -20,16 +19,12 @@ print('#####################')
 sum_list = sum(list_int)
 print(f'La suma de todos los numeros en la lista es: {sum_list}')
 
-check_num = int( input('Ingresa un numero para verificar su presencia en la lista: '))
-index_num = list_int.index(check_num)
-print(f'El numero {check_num} está presente en la pisición {index_num} de la lista')
+try:
+    check_num = int( input('Ingresa un numero para verificar su presencia en la lista: '))
+    index_num = list_int.index(check_num)
+    print(f'El numero {check_num} está presente en la pisición {index_num} de la lista')
+except ValueError:
+    print(f'El número {check_num} no se encuentra en la lista')
 
 list_int.pop()
 print(f'La lista actualizada despues de eliminar el ultimo elemento: {list_int}')
-
-"""
-
-print('--- Sistema de gestion de calificaciones ---\n 1.- Agregar nuevo estdiante\n 2.- Mostrar lista de estudiantes\n 3.- Buscar estudiante\n 4.- Eliminar estudiante\n 5.- Salir')
-
-option_selected = int(input('Ingresa una opcioón: '))
-
